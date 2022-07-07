b59b01602b9412691ae7978f3534c0a0ec7167d7
unknown <voyiaoo4>
2022-07-07 11:38:51 +0300

    added html syntax

--- edit-enroll.php
+++ edit-enroll.php
@@ -6,7 +6,10 @@
     while($fetchstudent =mysqli_fetch_array($querystudent))
     {
        $fullname = $fetchstudent['fullname'];
-
+       $phonenumber = $fetchstudent['phonenumber'];
+       $email = $fetchstudent['email'];
+       $gender = $fetchstudent['gender'];
+       $course = $fetchstudent['course'];
     }
 
 
@@ -18,6 +21,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.2.0/bootstrap-5.2.0-/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  
 </head>
 <body>
         <?php require_once('includes/header.php') ?>
@@ -35,14 +42,69 @@
                             <div class="card-header bg-dark text-center text-white">
                                 <h4> Edit student <?php echo $fullname ?></h4>
                             </div>
+                            <br><br>
+            <div class="card shadow" style="width:55rem; justify-content:center; margin-left:50vh;">
+                    <div class="card-body">
+                        <form action="enroll.php" method="POST">
+                            <div class="row">
+                                    <div class="mb-3 col-lg-6">
+                                            <label for="fullname" class="form-label">Fullname:</label>
+                                            <input type="text" name="fullname" class="form-control" placeholder="Enter your fullname" value="<?php echo $fullname ?>">
+                                        </div>
+                                    
+                                        <div class="mb-3 col-lg-6">
+                                            <label for="phonenumber" class="form-label">phonenumber</label>
+                                            <input type="tel" name="phonenumber" class="form-control" placeholder="phonenumber"value="<?php echo $phonenumber ?>" >
+                                        </div>
+                             </div>
+                                    <div class="row">
+                                        <div class="mb-3 col-lg-6">
+                                            <label for="email" class="form-label">email</label>
+                                            <input type="text" name="email" class="form-control" placeholder="Enter your email"value="<?php echo $email?>" >
+                                        </div>
+                                        <div class="mb-3 col-lg-6">
+                                            <label for="gender" class="form-label">gender</label>
+                                            <select class="form-control" aria-label="Default select example" name="gender" value="<?php echo $gender?>" >
+                                            <option selected style><?php echo $gender?></option>
+                                            <option value="male">male</option>
+                                            <option value="female">female</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                        <br><br>
+                                        
+                                        <div class="row" style="text-align:center:">
+                                    <div class="mb-3 col-lg-6">
+                                    <label for="course" class="form-label"><b>whats your preffered course</b></label>
+                                        <select class="form-select" aria-label="Default select example" name="course" value="<?php echo $course?>">
+                                        <option selected style><?php echo $course?></option>
+                                        <option value="data science">data science</option>
+                                        <option value="cyber security">cyber security</option>
+                                        <option value="software developement">software developement</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                          
+                                    <div class="row" style="text-align:center:">
+                                            <div class="mb-3 col-lg-6">
+                                                    <div class="mb-3 form-check">
+                                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                        <label class="form-check-label" for="exampleCheck1"><b>Agree Terms & Conditions</b></label>
+                                                    </div>
+                                                <br>      
+                                                <button type="submit" name="submitbutton" class="btn btn-primary">submit application</button>
+                                            </div>
+                                     </div>
+                  </form>       
+                </div>
+            </div>
+
                           </div>
                         </div>
                     </div>
                </div>
             </div>
-
-
-
+            <br><br>
 
 
 
