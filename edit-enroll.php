ee071c68c5d507215acbfd2ca2e7f60ab9b9ab24
unknown <voyiaoo4>
2022-07-08 11:14:20 +0300

    added htmml syntax

--- edit-enroll.php
+++ edit-enroll.php
@@ -9,8 +9,32 @@
        $phonenumber = $fetchstudent['phonenumber'];
        $email = $fetchstudent['email'];
        $gender = $fetchstudent['gender'];
-       $course = $fetchstudent['course'];
+     
     }
+    // updating user records
+    if(isset($_POST['updateenroll']))
+    {
+        //fetch form data
+        $fullname = $_POST['fullname'];
+        $phonenumber = $_POST['phonenumber'];
+       $email =  $_POST['email'];
+       $gender = $_POST['gender'];
+
+       //perform the sqli query
+    
+       $updateenroll = mysqli_query($conn, "UPDATE enroll SET fullname ='$fullname', phonenumber ='$phonenumber', email ='$email', gender ='$gender'
+       WHERE id='".$_GET['id']."'");
+
+       if($updateenroll)
+       {
+        message updated succesfully;
+       }
+       else
+     {
+        message not updated succesfully;
+     }
+    }
+
 
 
 ?>
@@ -39,72 +63,55 @@
                    <div class="row">
                         <div class="col-lg-12">
                           <div class="card">
-                            <div class="card-header bg-dark text-center text-white">
+                              <div class="card-header bg-dark text-center text-white">
                                 <h4> Edit student <?php echo $fullname ?></h4>
-                            </div>
-                            <br><br>
-            <div class="card shadow" style="width:55rem; justify-content:center; margin-left:50vh;">
-                    <div class="card-body">
-                        <form action="enroll.php" method="POST">
-                            <div class="row">
-                                    <div class="mb-3 col-lg-6">
-                                            <label for="fullname" class="form-label">Fullname:</label>
-                                            <input type="text" name="fullname" class="form-control" placeholder="Enter your fullname" value="<?php echo $fullname ?>">
-                                        </div>
-                                    
-                                        <div class="mb-3 col-lg-6">
-                                            <label for="phonenumber" class="form-label">phonenumber</label>
-                                            <input type="tel" name="phonenumber" class="form-control" placeholder="phonenumber"value="<?php echo $phonenumber ?>" >
-                                        </div>
-                             </div>
-                                    <div class="row">
-                                        <div class="mb-3 col-lg-6">
-                                            <label for="email" class="form-label">email</label>
-                                            <input type="text" name="email" class="form-control" placeholder="Enter your email"value="<?php echo $email?>" >
-                                        </div>
-                                        <div class="mb-3 col-lg-6">
-                                            <label for="gender" class="form-label">gender</label>
-                                            <select class="form-control" aria-label="Default select example" name="gender" value="<?php echo $gender?>" >
-                                            <option selected style><?php echo $gender?></option>
-                                            <option value="male">male</option>
-                                            <option value="female">female</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                        <br><br>
-                                        
-                                        <div class="row" style="text-align:center:">
-                                    <div class="mb-3 col-lg-6">
-                                    <label for="course" class="form-label"><b>whats your preffered course</b></label>
-                                        <select class="form-select" aria-label="Default select example" name="course" value="<?php echo $course?>">
-                                        <option selected style><?php echo $course?></option>
-                                        <option value="data science">data science</option>
-                                        <option value="cyber security">cyber security</option>
-                                        <option value="software developement">software developement</option>
-                                        </select>
-                                    </div>
-                                    </div>
-                          
-                                    <div class="row" style="text-align:center:">
-                                            <div class="mb-3 col-lg-6">
-                                                    <div class="mb-3 form-check">
-                                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                                        <label class="form-check-label" for="exampleCheck1"><b>Agree Terms & Conditions</b></label>
+                              </div>
+                               <br><br>
+                                    <form action="enroll.php" method="POST">
+                                        <div class="row">
+                                                <div class="mb-3 col-lg-6">
+                                                        <label for="fullname" class="form-label">Fullname:</label>
+                                                        <input type="text" name="fullname" class="form-control" placeholder="Enter your fullname" value="<?php echo $fullname ?>">
                                                     </div>
-                                                <br>      
-                                                <button type="submit" name="submitbutton" class="btn btn-primary">submit application</button>
-                                            </div>
-                                     </div>
-                  </form>       
-                </div>
-            </div>
-
-                          </div>
+                                                
+                                                    <div class="mb-3 col-lg-6">
+                                                        <label for="phonenumber" class="form-label">phonenumber</label>
+                                                        <input type="tel" name="phonenumber" class="form-control" placeholder="phonenumber"value="<?php echo $phonenumber ?>" >
+                                                    </div>
+                                        </div>
+                                                <div class="row">
+                                                    <div class="mb-3 col-lg-6">
+                                                        <label for="email" class="form-label">email</label>
+                                                        <input type="text" name="email" class="form-control" placeholder="Enter your email"value="<?php echo $email?>" >
+                                                    </div>
+                                                    <div class="mb-3 col-lg-6">
+                                                        <label for="gender" class="form-label">gender</label>
+                                                        <select class="form-control" aria-label="Default select example" name="gender" value="<?php echo $gender?>" >
+                                                        <option selected style><?php echo $gender?></option>
+                                                        <option value="male">male</option>
+                                                        <option value="female">female</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                    <br><br>
+                                                    
+                                                    <div class="row" style="text-align:center:">
+                                            
+                                                </div>
+                                    
+                                                <div class="row" style="text-align:center:">
+                                                        <div class="mb-3 col-lg-6">
+                                                            <br>      
+                                                            <button type="submit" name="submitbutton" class="btn btn-primary">update records</button>
+                                                        </div>
+                                                </div>
+                                    </form>                                                                    
+                            </div>
                         </div>
-                    </div>
-               </div>
-            </div>
-            <br><br>
+                   </div>
+                </div>
+            </div>    
+                        <br><br>
 
 
 
